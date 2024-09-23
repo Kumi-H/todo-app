03c2a4b9821d72bfcc142c3b6a513303cb484d7a
Kumi-H <935garden@gmail.com>
2024-09-23 14:06:59 +0900

    Node.js の型定義インストール

--- frontend/src/app/task/[id]/page.tsx
+++ frontend/src/app/task/[id]/page.tsx
@@ -1,17 +1,14 @@
-/**
- * 詳細ページ
- */
-'use client'
+"use client";
 import TopHeader from "../../../components/TopHeader";
 import Task from "../../../components/Task";
 
-const TaskPage = ( { params }: { params: { id: number } }) => {
+const TaskPage = ({ params }: { params: { id: number } }) => {
   return (
     <div>
       <TopHeader />
-      <Task id={params.id}/>
-    </div> 
+      <Task id={params.id} />
+    </div>
   );
-}
+};
 
 export default TaskPage;
